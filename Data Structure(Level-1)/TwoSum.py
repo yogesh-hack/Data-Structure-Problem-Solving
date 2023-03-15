e251a94ab3eb4f8419cd56f32e880995371db5cb
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-03-15 21:48:43 +0530

    Create

---
+++ Data Structure(Level-1)/TwoSum.py
@@ -0,0 +1,16 @@
+# problem : 1
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+#       set a dictionary {key : value}
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
