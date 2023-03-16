571d195c9fedd66a76a689b9ac85065158ed3345
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-03-16 19:04:49 +0530

    Create

---
+++ Data Structure/IntersectionOfTwoArrays.py
@@ -0,0 +1,25 @@
+# Problem : 350
+
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [4,9]
+# Explanation: [9,4] is also accepted.
+
+# Approach : sort & Two Pointer
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        
+        ans = []
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                ans.append(nums1[i])
+                i += 1
+                j += 1
+        return ans
