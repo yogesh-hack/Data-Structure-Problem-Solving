ec37ba41a5541e3aa00caadb63f32e33136d83ed
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-03-16 19:10:28 +0530

    Create

---
+++ Data Structure/BestTimeToSell.py
@@ -0,0 +1,21 @@
+# Problem : 121
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0 #Buy
+        right = 1 #Sell
+        max_profit = 0
+
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left]
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
