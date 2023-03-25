8536bff3e555719f770c673e30bf608d8d6d76c8
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-03-25 19:41:27 +0530

    Create  MaximumDepthofBinaryTree.py

---
+++ Data Structure/MaximumDepthofBinaryTree.py
@@ -0,0 +1,19 @@
+# Problem : 104
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 3
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root: return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            return max(left, right) + 1
+        return dfs(root)
