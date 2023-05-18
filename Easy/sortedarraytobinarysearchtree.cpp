aa5ce049af5ee3423fa7debc0d9ad8ec1ecea737
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-05-18 08:10:13 +0530

    Create sortedarraytobinarysearchtree.cpp

---
+++ Easy/sortedarraytobinarysearchtree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(vector<int>& nums,int start, int end){
+        if(start > end) return {};
+
+        if(start <= end){
+            int mid = start + (end-start)/2;
+            TreeNode* root = new TreeNode(nums[mid]);
+            root -> left = solve(nums,start,mid-1);
+            root -> right = solve(nums,mid+1,end);
+            return root;
+        }
+        return NULL;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int n = nums.size();
+        return solve(nums,0,n-1);
+    }
+};
